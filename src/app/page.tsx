0c89bfbcb7c0cdea42b990bba1745b36e6cd7be3
arifbiswas <mxgangstar310@gmail.com>
2025-10-17 00:30:11 +0600

    add

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="container mx-auto min-h-screen bg-base-200">
+      <div>
+        <div>
+          <div>
+            <h1 className="text-6xl font-bold text-primary font-inter">
+              Arif Biswas
+            </h1>
+            <p>React Native Developer | Software Engineer</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
