5f896ee7e0a4e3c141ea01d8f332b38252427002
arifbiswas <mxgangstar310@gmail.com>
2025-10-20 23:53:51 +0600

    okay make custom circle

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion } from "motion/react";
+
 const Home = () => {
   return (
     <section className="container mx-auto min-h-screen bg-base-200">
@@ -19,8 +23,37 @@ const Home = () => {
             <button className=" btn-outline">Contact Me</button>
           </div>
         </div>
-        <div>
-          <img src="/profile.png" alt="" />
+        <div className="flex justify-center items-center">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="bg-primary h-[75vh]  w-[75vh] rounded-full relative overflow-hidden"
+          >
+            <motion.img
+              src="/avatar-one.png"
+              className="h-full object-cover w-full absolute"
+              alt=""
+            />
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className="bg-blue-500 h-[30vh] top-0  w-[30vh] rounded-full absolute"
+            />
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className="bg-blue-500 h-[30vh] z-50 bottom-0 top-0 right-0 left-0 absolute w-[30vh] rounded-full "
+            />
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.1 }}
+              transition={{ duration: 0.5 }}
+              className="bg-blue-500 h-[30vh] bottom-0 right-0 w-[30vh] rounded-full absolute"
+            />
+          </motion.div>
         </div>
       </div>
     </section>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <section>
+      <p className="text-xs underline hover:cursor-pointer hover:text-primary font-jetbrains-mono  text-end absolute top-3 right-4 z-50">
+        extended version 2.0
+      </p>
+    </section>
+  );
+};
+
+export default Navbar;
